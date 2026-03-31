942dca70405050101f3e7967a73e45be64c0189e
b-arkell <broarkell2@gmail.com>
2026-03-31 16:10:51 -0400

    Added saving and loading for Schedule
    Added save and load for Schedule so we can have persistent booking schedule

--- src/Scheduling/Scheduler.java
+++ src/Scheduling/Scheduler.java
@@ -1,17 +1,29 @@
 package Scheduling;
 
 import java.util.ArrayList;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
-public class Scheduler {
+public class Scheduler implements Serializable{
 
+	private static final long serialVersionUID = 1L;
+	
 	private Map<String, List<TimeSlot>> approved = new HashMap<>();
 	private Map<String, List<TimeSlot>> pending = new HashMap<>();	
 	
 	// User creates request -> adds to pending
 	public void addPending(String resourceId, TimeSlot slot) {
+		System.out.println("[Scheduler] Adding a pending resource for user: " + slot.getUserId() + " From: " + slot.getStart() + " To: " + slot.getEnd());
 		pending.computeIfAbsent(resourceId, k -> new ArrayList<>()).add(slot);
 	}
 	
@@ -21,9 +33,11 @@ public class Scheduler {
 		
 		for (TimeSlot s : slots) {
 			if (s.conflictsWith(slot)) {
+				System.out.println("[Scheduler] Resource: " + resourceId + " Has Overlap.");
 				return false;
 			}
 		}
+		System.out.println("[Scheduler] Resource: " + resourceId + " Has no Overlap.");
 		return true;
 	}
 	
@@ -31,12 +45,53 @@ public class Scheduler {
 	public void approve(String resourceId, TimeSlot slot) {
 		approved.computeIfAbsent(resourceId,  k -> new ArrayList<>()).add(slot);
 		pending.getOrDefault(resourceId, new ArrayList<>()).remove(slot);
+		System.out.println("[Scheduler] Moving resource: " + resourceId + " from Pending to Approved...");
 	}
 	
 	// remove from pending/reject
 	public void reject(String resourceId, TimeSlot slot) {
 		pending.getOrDefault(resourceId, new ArrayList<>()).remove(slot);
+		System.out.println("[Scheduler] Rejecting resource: " + resourceId + " and Removing From Pending...");
 	}
+	
+	public void saveToFile(String filename) {
+		try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filename))){
+			out.writeObject(approved);
+			out.writeObject(pending);
+			System.out.println("[Scheduler] saving schedule to file " + filename);
+		}catch (IOException e){
+			System.err.println("[Scheduler] failed to save schedule to file " + filename);
+			e.printStackTrace();
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void loadFromFile(String filename) throws ClassNotFoundException {
+		try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename))){
+			approved = (Map<String, List<TimeSlot>>) in.readObject();
+			pending = (Map<String, List<TimeSlot>>) in.readObject();
+			System.out.println("[Scheduler] Loading schedule from file: " + filename);
+		}catch (FileNotFoundException e) {
+			System.err.println("[Scheduler] Failed to find file: " + filename);
+			e.printStackTrace();
+		}catch (IOException | ClassNotFoundException e) {
+			System.out.println("[Scheduler] IOException or Class not Found for file: " + filename);
+			e.printStackTrace();
+		}
+	}
+	
+	public void clearScheduleData() throws FileNotFoundException, IOException {
+		File file = new File("scheduler.dat");
+		try (FileOutputStream fos = new FileOutputStream(file)){
+			// writing nothing here will write an empty file to replace
+			System.out.println("[Scheduler] Scheduler data cleared (file now empty).");
+		} catch (IOException e) {
+			System.out.println("[Scheduler] Error while clearing schedule file...");
+			e.printStackTrace();
+		}
+
+	}
+	
 	
 	
 }
--- src/Scheduling/TestSchedulingMain.java
+++ src/Scheduling/TestSchedulingMain.java
@@ -1,21 +1,31 @@
 package Scheduling;
 
+import java.io.File;
 import java.time.LocalDateTime;
 import Booking.*;
 import ResourceStates.*;
 
 public class TestSchedulingMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException {
 		
 		BookingService bookingService = new BookingService();
 		Scheduler scheduler = new Scheduler();
+		
+		File file = new File("scheduler.dat");
+		if (!file.exists() || file.length() == 0) {
+			System.out.println("No Previous Scheduler Data. Starting Fresh Schedule...");
+		}else {
+			scheduler.loadFromFile("scheduler.dat");			
+		}
+		
+		
 		
 		// create resource
 		IResource room = ResourceFactory.createResource("Study Room 101",new RoomBookingStrategy());
 		
 		// create time slot			// int year, int month, int dayOfMonth, int hour, int minute
-		TimeSlot slot = new TimeSlot(LocalDateTime.of(2026,4, 1, 10, 0), LocalDateTime.of(2026,  4, 1, 12, 0));
+		TimeSlot slot = new TimeSlot(LocalDateTime.of(2026,4, 1, 10, 0), LocalDateTime.of(2026,  4, 1, 12, 0), "User1");
 		
 		
 		/// USER ACTION
@@ -33,11 +43,12 @@ public class TestSchedulingMain {
 		
 		if (scheduler.canApprove(room.getName(), slot)){
 			
-			System.out.println("Admin approves booking...");
-			
-			scheduler.approve(room.getName(), slot);
-			
 			bookingService.BookResource(room, "User1");
+			scheduler.approve(room.getName(), slot);
+			System.out.println("Admin approves booking...");
+
+			
+
 		}else {
 			System.out.println("Admin rejects Booking...");
 			scheduler.reject(room.getName(), slot);
@@ -49,7 +60,7 @@ public class TestSchedulingMain {
 
         TimeSlot slot2 = new TimeSlot(
                 LocalDateTime.of(2026, 4, 1, 11, 0),
-                LocalDateTime.of(2026, 4, 1, 13, 0)
+                LocalDateTime.of(2026, 4, 1, 13, 0), "User2"
         );
 
         scheduler.addPending(room.getName(), slot2);
@@ -73,6 +84,8 @@ public class TestSchedulingMain {
             scheduler.reject(room.getName(), slot2);
             room.setState(new AvailableState());
         }
+        
+        scheduler.saveToFile("scheduler.dat");
         
 		
 	}
